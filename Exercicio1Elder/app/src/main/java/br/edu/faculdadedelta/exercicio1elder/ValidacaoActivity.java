5c4d198b3eeee030f33eee5ea14c3d3449b5e70f
Elder <eldertec@gmail.com>
2019-03-25 15:28:44 -0300

    Regras de negócio no Exercício 1

--- Exercicio1Elder/app/src/main/java/br/edu/faculdadedelta/exercicio1elder/ValidacaoActivity.java
+++ Exercicio1Elder/app/src/main/java/br/edu/faculdadedelta/exercicio1elder/ValidacaoActivity.java
@@ -3,7 +3,10 @@ package br.edu.faculdadedelta.exercicio1elder;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import java.util.Date;
 
 public class ValidacaoActivity extends AppCompatActivity {
 
@@ -38,10 +41,39 @@ public class ValidacaoActivity extends AppCompatActivity {
             peso = intent.getStringExtra("pesoParam");
             altura = intent.getStringExtra("alturaParam");
 
-            tvNome.setText(nome);
-            tvDtNascimento.setText(dtNascimento);
-            tvPeso.setText(peso);
-            tvAltura.setText(altura);
+            tvNome.setText("Nome: " + nome);
+            tvDtNascimento.setText("Data de Nascimento: " + dtNascimento);
+            tvPeso.setText("Peso: " + peso);
+            tvAltura.setText("Altura: " + altura);
         }
+    }
+
+    public void validar(View view){
+        String msgRetorno = "";
+        double pesoTemp = Double.parseDouble(peso);
+        Date dataTemp = new Date(dtNascimento);
+        Date hoje = new Date();
+
+        if(nome.equals("")){
+            msgRetorno = "O nome é obrigatório!";
+        }else if(dtNascimento.equals("")){
+            msgRetorno += "\nA data de nascimento é obrigatória!";
+        }else if(peso.equals("")){
+            msgRetorno += "\nO peso é obrigatório!";
+        }else if(altura.equals("")){
+            msgRetorno += "\nA altura é obrigatória";
+        }else if(pesoTemp <= 60.4){
+            msgRetorno += "\nO peso deve ser maior que 60,4Kg";
+        }else if(nome.length() <= 30){
+            msgRetorno += "\nO nome deve ter mais que 30 caracteres!";
+        }else if(dataTemp.before(hoje)){
+            msgRetorno += "\nA data de nascimento deve ser anterior a data atual!";
+        }
+
+        Intent intent = new Intent();
+        intent.putExtra("msgRetorno", msgRetorno);
+
+        setResult(RESULT_SUCESS, intent);
+        finish();
     }
 }
